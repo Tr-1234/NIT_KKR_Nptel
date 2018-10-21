5b976c410f5a4761e682c76020fc85ee875cd9c6
akash1309 <akash.goyal1309@gmail.com>
2018-10-21 12:45:28 +0530

    logins.api made

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import spider from './Videos/spider.mp4';
-class App extends Component {
-   render() {
-      return (
-        <div>
-           <h2>NPTEL Videos</h2>
-           <p>Author: </p>
-           <p>Title: </p>
-           <button> Get Video </button>
-           <br/><br/><br/>
-            <video
-              controls
-              src={spider}
-            />
-        </div>
-      );
-   }
-}
-export default App;
+import React from 'react';
+import {
+    Redirect,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import Login from './logins';
+
+export default function App(props) {
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact component={Login} />
+      </Switch>
+    </div>
+  )
+};
---
+++ logins.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {
+    Redirect,
+    Route,
+    Switch,
+    Link
+} from 'react-router-dom';
+
+import axios from 'axios';
+import {
+  AppBar,
+  RaisedButton,
+  TextField
+} from 'material-ui';
+
+class Login extends Component {
+
+  constructor(props){
+    super(props);
+    console.log('Login', props);
+    this.state={
+      name:'',
+      password:'',
+      flag: -1,
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+
+            <AppBar title="Login"/>
+
+            <div style={styles.outerContainerStyle}>
+              <div style={styles.innerContainerStyle}>
+                <TextField
+                  hintText="Enter your name"
+                  floatingLabelText="Name"
+                  onChange = {(event,newValue) => this.setState({name:newValue, flag: -1})}
+                  style={{ marginTop: 10 }}
+                />
+                <TextField
+                  type="password"
+                  hintText="Enter Password"
+                  floatingLabelText="Password"
+                  onChange = {(event,newValue) => this.setState({password:newValue})}
+                  style={{ marginTop: -10 }}
+                />
+                <br/>
+                <RaisedButton label="Login" primary={true} style={styles.buttonStyle} />
+              </div>
+            </div>
+
+          </div>
+
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+
+
+  }
+
+  const styles = {
+    outerContainerStyle: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    innerContainerStyle: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '2px solid #00BCD4',
+      borderRadius: 25,
+      margin: 70,
+      padding: 30
+    },
+    buttonStyle: {
+      margin: 15
+    }
+  };
+
+  export default Login;
